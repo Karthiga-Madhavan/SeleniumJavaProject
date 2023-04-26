b1e723e5b9170f5f26d538763249cd0b222685ea
Karthiga-Madhavan <124021031+Karthiga-Madhavan@users.noreply.github.com>
2023-04-27 00:18:37 +0530

    finding xpath

--- src/day01/Program01.java
+++ src/day01/Program01.java
@@ -3,15 +3,19 @@ package day01;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Program01 {
 
 	public static void main(String[] args) {
 	 WebDriver driver;	
 	 
-	 driver = new ChromeDriver();
+	 ChromeOptions options=new ChromeOptions();
+	 options.addArguments("--remote-allow-origins=*");
+	 
+	 driver = new ChromeDriver(options);
 	 driver.get("https://demo.automationtesting.in/Register.html");	
-	 /*driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Test");
+	 driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Test");
 	 driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Java");
 	 driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("Chennai");
 	 
@@ -19,7 +23,7 @@ public class Program01 {
 	 
 	 driver.findElement(By.xpath("//input[@value='Male']")).click(); 
 	 
-	// driver.findElement(By.xpath("//button[@id='Button1']")).submit();	 */
+	 driver.findElement(By.xpath("//button[@value='Refresh']")).submit();
 	 
 	}
 
